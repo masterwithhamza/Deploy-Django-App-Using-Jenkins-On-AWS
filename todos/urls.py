1580b91d22c0e5ef6c554911f0476a5536d31bf6
Hamza Rehman <128228180+hamzarehmansheikh4@users.noreply.github.com>
2023-06-08 09:34:18 +0500

    Update urls.py

--- todos/urls.py
+++ todos/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name='todos'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('<int:todo_id>/delete', views.delete, name='delete'),
-    path('<int:todo_id>/update', views.update, name='update'),
-    path('add/', views.add, name='add')
+    path('<int:todo_id>/delete/$', views.delete, name='delete'),
+    path('<int:todo_id>/update/$', views.update, name='update'),
+    path('add/$', views.add, name='add')
 ]
